acb68a100b15fbbd63970962fabfc9a0aad529ac
Myles <43725835+MylesAndMore@users.noreply.github.com>
2026-05-14 17:40:22 -0500

    add roof endpoint

--- src/lib/devices.ts
+++ src/lib/devices.ts
@@ -9,6 +9,12 @@ export const DEVICE_LOCATIONS: DeviceLocation[] = [
         lat: 41.90757477534645,
         lng: -87.65264276429056,
     },
+    {
+        endpoint: "30eda0a5f780",
+        label: "Roof",
+        lat: 41.91427793569204,
+        lng: -87.6881696515973,
+    },
     // Add more devices here as they are deployed
 ];
 
