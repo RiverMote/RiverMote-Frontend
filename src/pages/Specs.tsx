d4d82d9ce8f9ae5a5807b4928ab42db4461e8c75
rdrink <rdrink@protonmail.com>
2026-06-03 16:50:26 -0500

    RD minor edits

---
+++ src/pages/Specs.tsx
@@ -0,0 +1,7 @@
+export default function Specs() {
+    return (
+        <div className="max-w-3xl mx-auto px-6 py-20">
+            <div className="mb-14 text-center"></div>
+        </div>
+    );
+}
